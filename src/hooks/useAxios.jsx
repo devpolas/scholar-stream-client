d0119e485a314756ba74ce526a0d507a0e59d1d0
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-12-11 19:46:03 +0600

    implement axios instance

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useAuthContext from "./../contexts/useAuthContext";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8000/",
+});
+
+export default function useAxiosSecure() {
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    instance.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+  }, [user]);
+
+  return instance;
+}
