f09be24f5695693e4778a163e3ae321c2d017f15
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-12-16 00:44:56 +0600

    set optional for function call

--- src/components/ui/ActionsButtons.jsx
+++ src/components/ui/ActionsButtons.jsx
@@ -19,7 +19,7 @@ export default function ActionsButtons({
   return (
     <div className='flex gap-2 justify-center items-center text-sm lg:text-lg'>
       {paymentStatus === "unpaid" && (
-        <button onClick={onPay} title='Pay'>
+        <button onClick={() => onPay?.()} title='Pay'>
           <MdPayment className='cursor-pointer hover:text-green-600' />
         </button>
       )}
@@ -27,23 +27,23 @@ export default function ActionsButtons({
       {(role === "admin" ||
         role === "moderator" ||
         applicationStatus === "pending") && (
-        <button onClick={onDelete} title='Delete'>
+        <button onClick={() => onDelete?.()} title='Delete'>
           <FaRegTrashAlt className='cursor-pointer hover:text-red-600' />
         </button>
       )}
 
       {applicationStatus === "completed" && (
-        <button onClick={onComment} title='Add Comment'>
+        <button onClick={() => onComment?.()} title='Add Comment'>
           <BiCommentAdd className='cursor-pointer hover:text-blue-600' />
         </button>
       )}
 
-      <button onClick={onView} title='View Info'>
+      <button onClick={() => onView?.()} title='View Info'>
         <BsInfoCircle className='cursor-pointer hover:text-gray-600' />
       </button>
 
       {(role === "admin" || role === "moderator") && (
-        <button onClick={onEdit} title='Edit'>
+        <button onClick={() => onEdit?.()} title='Edit'>
           <FaEdit className='cursor-pointer hover:text-yellow-600' />
         </button>
       )}
